98bda0e5ae46bd334b12420edceb9db468f567df
plocket <52798256+plocket@users.noreply.github.com>
2020-10-17 18:33:34 -0400

    Try env var

--- copy_replace_generate.sh
+++ copy_replace_generate.sh
@@ -3,4 +3,4 @@
 # If folder doesn't exist, make folder
 mkdir output
 
-sed 's/github_url/$INPUT_DESTINATION_REPO/g' to_copy_and_change.json > output/output_file_01.json
+sed 's/github_url/$DESTINATION_REPO/g' to_copy_and_change.json > output/output_file_01.json
