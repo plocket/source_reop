dc964c3447d0e34c44ca1ac6a2123b693a208136
plocket <52798256+plocket@users.noreply.github.com>
2020-10-17 19:22:15 -0400

    Output file again

--- copy_replace_generate.sh
+++ copy_replace_generate.sh
@@ -3,4 +3,4 @@
 # If folder doesn't exist, make folder
 mkdir output
 
-sed -i 's|github_url|'"$DESTINATION_REPO"'|g' to_copy_and_change.json > output/output_file_01.json
+sed 's|github_url|'"$DESTINATION_REPO"'|g' to_copy_and_change.json > output/output_file_01.json
