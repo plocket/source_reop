55b151a73116e89c44db20826a57f1e792a3315c
plocket <52798256+plocket@users.noreply.github.com>
2020-10-17 17:07:05 -0400

    Try outputting file differently

--- copy_replace_generate.sh
+++ copy_replace_generate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # If folder doesn't exist, make folder
-mkdir output2
+mkdir output
 
-sed 's/github_url/$INPUT_DESTINATION_REPO/g' to_copy_and_change.json > output2/output.json
+sed 's/github_url/$INPUT_DESTINATION_REPO/g' to_copy_and_change.json > output/output.json
+cat output/output.json
